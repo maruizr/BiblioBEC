9e03a270a90e853ae5a050c688b6ef7191759735
maruizr <maribelruizr303@gmail.com>
2021-06-08 14:08:59 -0400

    Se ingresa validaciones de ingreso básicas para el login, se agrega la página principal con boton de cerrar sesión, cambios en el action bar y se añade logo en página principal

---
+++ app/src/main/java/com/example/bibliobec/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.bibliobec;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar mibarra= (Toolbar) findViewById(R.id.miBarra);
+        setSupportActionBar(mibarra);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.actionLogOut){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/bibliobec/LoginActivity.java
+++ app/src/main/java/com/example/bibliobec/LoginActivity.java
@@ -3,18 +3,46 @@ package com.example.bibliobec;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginRut, loginPass;
+    private Button loginBtn;
+    private TextView forgotPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Login");
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+        loginRut = findViewById(R.id.logRut);
+        loginPass = findViewById(R.id.logPassword);
+        loginBtn = findViewById(R.id.logBtn);
+        forgotPass = findViewById(R.id.forgetPass);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rut = loginRut.getText().toString().trim();
+                String password = loginPass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(rut)){
+                    loginRut.setError("Debe ingresar rut");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    loginPass.setError("Debe ingresar su contraseña");
+                }
+                else{
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                }
+            }
+        });
     }
 }
