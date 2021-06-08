efd60dfa7d0aef3f04d7c61d44993a6f7ab02757
maruizr <maribelruizr303@gmail.com>
2021-06-08 12:45:42 -0400

    Cambio en los nombres de los activity para el login

---
+++ app/src/main/java/com/example/bibliobec/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.bibliobec;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Login");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+}
